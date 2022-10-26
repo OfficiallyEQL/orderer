7f65cd7014839b97e8a685534a62380059d47b5b
Julia Ogris <julia.ogris@gmail.com>
2022-10-26 15:18:46 +1100

    go: Add basic usage of go-shopify client
    Add basic usage of go-shopify client. Setup end-to-end testing querying
    real Shopify store as set up in environment with the following
    variables:
    
    	SHOPIFY_TOKEN=shpat_6a......5470
    	SHOPIFY_STORE=julias-delights

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/alecthomas/kong"
+	goshopify "github.com/bold-commerce/go-shopify/v3"
 )
 
 var (
@@ -15,22 +16,24 @@ var (
 	date    = "now"
 
 	description = `
-orderer imports orders into shopify store
+orderer imports orders into Shopify store
 `
 	cli struct {
 		Config
-		Version kong.VersionFlag `help:"Show version."`
+		Version kong.VersionFlag `help:"Show version." env:"-"`
 	}
 )
 
 type Config struct {
-	Store string `arg:"" required:"" help:"Shopify store name as found in myshopify.com URL"`
+	Store string `required:"" help:"Shopify store name as found in <name>.myshopify.com URL."`
+	Token string `required:"" help:"Shopify Admin token."`
 	out   io.Writer
 }
 
 func main() {
 	kctx := kong.Parse(&cli,
 		kong.Description(description),
+		kong.DefaultEnvars("shopify"),
 		kong.Vars{"version": fmt.Sprintf("%s (%s on %s)", version, commit, date)},
 	)
 	kctx.FatalIfErrorf(kctx.Run())
@@ -42,6 +45,13 @@ func (c *Config) AfterApply() error {
 }
 
 func (c *Config) Run() error {
+	client := goshopify.NewClient(goshopify.App{}, c.Store, c.Token)
+
+	orders, err := client.Order.List(nil)
+	if err != nil {
+		return err
+	}
 	fmt.Fprintf(c.out, "Syncing order for store %q\n", c.Store)
+	fmt.Fprintf(c.out, "Order count: %d\n", len(orders))
 	return nil
 }
